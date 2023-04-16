e83763e038c61509cc66f831dbfadb0806835878
Clinton <clintoncky@gmail.com>
2023-04-16 14:32:08 +0800

    store the merkle root, verify the proof and name from api

--- server/index.js
+++ server/index.js
@@ -8,19 +8,18 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '2827e12064520ace62774f6254c680ebd7d451bbc9f060e8a1b23a2213882817';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send('You got a toy robot!');
+  } else {
+    res.send('You are not on the list :(');
   }
 });
 
